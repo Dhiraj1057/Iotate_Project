0466653b928e46a96ac446ac22c76f9daad7c05b
Dhiraj Kumar <kumar.dhiraj970@gmail.com>
2023-12-07 22:56:20 +0530

    Bug Fix

--- FlareSens/static/js/Site.js
+++ FlareSens/static/js/Site.js
@@ -1,5 +1,32 @@
 "use strict";
 
+class HeatMap {
+    #heatmapContainer;
+    #imageElement
+    #datapoint
+    #heatmap;
+    constructor(heatmapContainer, datapoint) {
+        this.#heatmapContainer = heatmapContainer;
+        this.#imageElement = this.#heatmapContainer.querySelector("img")
+        this.#datapoint = datapoint;
+        let cfg = {
+            container: this.#heatmapContainer,
+            radius: 20, // Adjust the radius as needed
+        }
+        this.#heatmap = h337.create(cfg);
+        this.update();
+        window.addEventListener("resize", () => this.update())
+    }
+
+    update() {
+        let ratio = this.#imageElement.clientWidth / this.#imageElement.naturalWidth;
+        let paddingWidth = (this.#heatmapContainer.clientWidth - this.#imageElement.clientWidth) / 2;
+        let paddingHeight = (this.#heatmapContainer.clientHeight - this.#imageElement.clientHeight) / 2;
+        let datapoint = this.#datapoint.map(value => ({ x: Math.round(ratio * value.X + paddingWidth), y: Math.round(ratio * value.X + paddingHeight), value: 10 }))
+        this.#heatmap.setData({ data: datapoint });
+    }
+}
+
 function scrollList(direction) {
     const scrollAmount = 200; // Adjust as needed
     const itemList = document.querySelector('.FlareSens-Scroll-Container>article');
@@ -17,5 +44,8 @@ document.addEventListener("readystatechange", function () {
         // code here
     } else if (this.readyState === "complete") {
         // code here
+        new HeatMap(document.querySelector("#heatmap-container"), [{ X: 225, Y: 225 }])
     }
 });
+
+
