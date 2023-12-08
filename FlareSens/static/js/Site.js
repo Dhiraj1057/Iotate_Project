f92335bf93c5c4a7e4af29794da36e34862aa582
Dhiraj Kumar <kumar.dhiraj970@gmail.com>
2023-12-08 16:29:44 +0530

    updation

--- FlareSens/static/js/Site.js
+++ FlareSens/static/js/Site.js
@@ -5,25 +5,51 @@ class HeatMap {
     #imageElement
     #datapoint
     #heatmap;
-    constructor(heatmapContainer, datapoint) {
+    #isShow = true;
+    constructor(heatmapContainer) {
         this.#heatmapContainer = heatmapContainer;
         this.#imageElement = this.#heatmapContainer.querySelector("img")
-        this.#datapoint = datapoint;
         let cfg = {
             container: this.#heatmapContainer,
             radius: 20, // Adjust the radius as needed
         }
         this.#heatmap = h337.create(cfg);
-        this.update();
-        window.addEventListener("resize", () => this.update())
+        this.#autoUpdate()
+        window.addEventListener("resize", () => this.#update())
     }
 
-    update() {
+    changeImage(imageElement) {
+        this.#imageElement = imageElement;
+        this.#autoUpdate();
+    }
+
+    changeDatapoint(datapoint) {
+        this.#datapoint = datapoint;
+        this.#update();
+    }
+
+    #autoUpdate() {
+        let inputString = this.#imageElement.getAttribute("data-point");
+        const validJsonString = inputString.replace(/'/g, "\"");
+        this.#datapoint = JSON.parse(validJsonString);
+        this.#update();
+    }
+
+    #update() {
         let ratio = this.#imageElement.clientWidth / this.#imageElement.naturalWidth;
-        let paddingWidth = (this.#heatmapContainer.clientWidth - this.#imageElement.clientWidth) / 2;
+        this.#heatmapContainer.querySelector("canvas").setAttribute("width", this.#imageElement.clientWidth);
+        this.#heatmapContainer.querySelector("canvas").setAttribute("height", this.#heatmapContainer.clientHeight);
         let paddingHeight = (this.#heatmapContainer.clientHeight - this.#imageElement.clientHeight) / 2;
-        let datapoint = this.#datapoint.map(value => ({ x: Math.round(ratio * value.X + paddingWidth), y: Math.round(ratio * value.X + paddingHeight), value: 10 }))
+        let datapoint = [];
+        if (this.#isShow) {
+            datapoint = this.#datapoint.map(value => ({ x: Math.round(ratio * value.X), y: Math.round(ratio * value.Y + paddingHeight), value: 10 }))
+        }
         this.#heatmap.setData({ data: datapoint });
+    }
+
+    toggle() {
+        this.#isShow = !this.#isShow;
+        this.#update();
     }
 }
 
@@ -44,7 +70,10 @@ document.addEventListener("readystatechange", function () {
         // code here
     } else if (this.readyState === "complete") {
         // code here
-        new HeatMap(document.querySelector("#heatmap-container"), [{ X: 225, Y: 225 }])
+        let heatmap = new HeatMap(document.querySelector("#heatmap-container"));
+        document.querySelector(".FlareSens-Button-Full").addEventListener("click", () => {
+            heatmap.toggle()
+        })
     }
 });
 
